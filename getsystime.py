5efe190efabf9e0352ef66c9a559287b2e9dceb8
egilson <gilson.eric@gmail.com>
2019-09-13 19:50:29 +0200

    Add the code in the main file to show in browser a jpeg file and I added a sample file to test this nez behavior.

--- getsystime.py
+++ getsystime.py
@@ -1,4 +1,10 @@
+import os
+import webbrowser
+
 from datetime import date
 
 today = "Today's date : " + str(date.today())
 print(today)
+
+strFileNamePath = os.getcwd() + "/python.jpg"
+webbrowser.open(strFileNamePath)
