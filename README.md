e3d8d49a4ca4d265483422a57d8902331340eb7a
egilson <gilson.eric@gmail.com>
2019-09-13 19:13:56 +0200

    Update the main file code to show the actual time.

--- README.md
+++ README.md
@@ -1,2 +1,4 @@
 # getsystime
 Show the current system time.
+#Author : Eric GILSON
+#Date   : Sept. 2019
---
+++ getsystime.py
@@ -0,0 +1,4 @@
+from datetime import date
+
+today = "Today's date : " + str(date.today())
+print(today)
